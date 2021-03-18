ff999e098bc6a8f1c3eb85bc2b893ca11a685339
Colin Mudie <csmudie1@gmail.com>
2021-03-17 21:35:33 -0400

    added event listeners to character select screen

--- public/js/crafty.js
+++ public/js/crafty.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-empty-function */
+// eslint-disable-next-line no-unused-vars
+const character = this.currentCharacter;
+
 window.onload = function() {
   //start crafty
+  console.log(this.currentCharacter);
   Crafty.init(50, 400, 320);
   Crafty.canvas();
 };
